c3dd17a1a2103de611192bed4998869e38b8342c
Kaito Imanishi <societyoutfit1412IK@gmail.com>
2023-07-09 00:11:52 +0900

    Monsterクラスのメソッドの追加

--- app/src/test/java/semonster2/MonsterTest.java
+++ app/src/test/java/semonster2/MonsterTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MonsterTest {
-    
-  @Test  
-  public void TestToString() {
-    
-    Monster monster = new Monster("デュラハン",0);
-    assertEquals("デュラハン:レア度[0]\n", monster.toString());
 
+  @Test
+  public void TestToString() {
+
+    Monster classUnderTest_monster = new Monster("スライム", 3);
+    String String_expected = "強スライム:レア度[3]\n";
+    assertEquals(String_expected, classUnderTest_monster.toString());
   }
+
+  @Test
+  public void TestSummonMonster() {
+
+    Monster classUnderTest_monster = new Monster(0);
+    String String_expected = "スライム:レア度[0]\n";
+    assertEquals(String_expected, classUnderTest_monster.toString());
+  }
+
 }
